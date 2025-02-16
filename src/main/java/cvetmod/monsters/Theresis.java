6341662aa6c097055ead45c82939141c733f41f1
absi2011 <absi2011@126.com>
2025-02-17 00:03:43 +0800

    大特老师

---
+++ src/main/java/cvetmod/monsters/Theresis.java
@@ -0,0 +1,129 @@
+package cvetmod.monsters;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.MonsterStrings;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import cvetmod.powers.ChessPower;
+
+import java.util.ArrayList;
+
+public class Theresis extends AbstractCvetMonster {
+    public static final String ID = "cvetmod:Theresis";
+    public static final MonsterStrings monsterStrings = CardCrawlGame.languagePack.getMonsterStrings(ID);
+    public static final String[] MOVES = monsterStrings.MOVES;
+    public static final String NAME = monsterStrings.NAME;
+    public boolean isUpgrade = false;
+    public int blockGain;
+    public int strGain;
+
+    public Theresis(float x, float y) {
+        super(NAME, ID, 400, 0, 0, 150.0F, 320.0F, null, x, y);
+        type = EnemyType.BOSS;
+        if (AbstractDungeon.ascensionLevel >= 9) {
+            blockGain = 5;
+            setHp(420);
+        }
+        else {
+            blockGain = 4;
+        }
+        if (AbstractDungeon.ascensionLevel >= 19) {
+            damage.add(new DamageInfo(this, 3));
+            damage.add(new DamageInfo(this, 2));
+            strGain = 2;
+        } else if (AbstractDungeon.ascensionLevel >= 4) {
+            damage.add(new DamageInfo(this, 2));
+            damage.add(new DamageInfo(this, 1));
+            strGain = 2;
+        } else {
+            damage.add(new DamageInfo(this, 2));
+            damage.add(new DamageInfo(this, 1));
+            strGain = 1;
+        }
+//        loadAnimation("cvetmod/images/monsters/enemy_1327_cbrokt/enemy_1327_cbrokt33.atlas", "cvetmod/images/monsters/enemy_1327_cbrokt/enemy_1327_cbrokt33.json", 1.5F);
+//        state.setAnimation(0, "Idle_1", true);
+        isUpgrade = false;
+        flipHorizontal = true;
+    }
+
+    @Override
+    public void usePreBattleAction() {
+        addToBot(new ApplyPowerAction(this, this, new ChessPower(this)));
+    }
+
+    @Override
+    public void takeTurn() {
+        AbstractPlayer p = AbstractDungeon.player;
+        if (nextMove == 1) {
+            addToBot(new DamageAction(p, new DamageInfo(this, damage.get(0).output)));
+            if (damage.get(0).output <= damage.get(0).base) {
+                isUpgrade = true;
+            }
+        }
+        else if (nextMove == 2) {
+            addToBot(new GainBlockAction(this, this, blockGain));
+        }
+        else if (nextMove == 3) {
+            addToBot(new DamageAction(p, new DamageInfo(this, damage.get(1).output)));
+            addToBot(new DamageAction(p, new DamageInfo(this, damage.get(1).output)));
+            if (damage.get(1).output <= damage.get(1).base) {
+                isUpgrade = true;
+            }
+        }
+        else if (nextMove == 4) {
+            addToBot(new ApplyPowerAction(p, this, new WeakPower(p, 1, false)));
+        }
+        else {
+            addToBot(new ApplyPowerAction(this, this, new StrengthPower(this, strGain)));
+        }
+        getMove(0);
+        isUpgrade = false;
+    }
+
+    @Override
+    protected void getMove(int i) {
+        ArrayList<Integer> possibleMoves = new ArrayList<>();
+        if (isUpgrade) {
+            possibleMoves.add(5);
+        }
+        else {
+            if (nextMove != 1) {
+                possibleMoves.add(1);
+            }
+            if (nextMove != 2) {
+                possibleMoves.add(2);
+            }
+            if (nextMove != 3) {
+                possibleMoves.add(3);
+            }
+            if ((nextMove != 4) && (!AbstractDungeon.player.hasPower(WeakPower.POWER_ID))) {
+                possibleMoves.add(4);
+            }
+            if (nextMove != 5) {
+                possibleMoves.add(5);
+            }
+        }
+        int move = possibleMoves.get(AbstractDungeon.aiRng.random(possibleMoves.size() - 1));
+        if (move == 1) {
+            setMove((byte)1, Intent.ATTACK, damage.get(0).base);
+        }
+        else if (move == 2) {
+            setMove((byte)2, Intent.DEFEND);
+        }
+        else if (move == 3) {
+            setMove((byte)3, Intent.ATTACK, damage.get(1).base, 2, true);
+        }
+        else if (move == 4) {
+            setMove((byte)4, Intent.DEBUFF);
+        }
+        else if (move == 5) {
+            setMove((byte)5, Intent.BUFF);
+        }
+    }
+}
---
+++ src/main/java/cvetmod/powers/ChessPower.java
@@ -0,0 +1,36 @@
+package cvetmod.powers;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class ChessPower extends AbstractCvetPower {
+    public static final String POWER_ID = "cvetmod:ChessPower";
+    public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    public ChessPower(AbstractCreature owner) {
+        this.ID = POWER_ID;
+        this.name = NAME;
+        this.type = PowerType.BUFF;
+        this.owner = owner;
+        //TODO: 找个机会创建个模板吧，别非要让我用cards的
+        region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("cvetmod/images/cards/CvetStrikeA.png"), 0, 0, 128, 128);
+        region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("cvetmod/images/cards/CvetStrikeA.png"), 0, 0, 48, 48);
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0];
+    }
+
+    @Override
+    public void onAfterCardPlayed(AbstractCard usedCard) {
+        ((AbstractMonster)owner).takeTurn();
+    }
+}
